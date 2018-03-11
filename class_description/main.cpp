f8bfa59161b6311af7104ad6268312fab33a9a50
Aniket Kumar <cs5160398@cse.iitd.ac.in>
2018-03-11 18:11:05 +0530

    Point and object made and translation and rotation_z done

--- class_description/main.cpp
+++ class_description/main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 
 int main(){
 	Object3D o = Object3D("obj.txt");
-	int len;
-	len = o.edges();
-	printf ("%d", len);
+	o.vertices();
+	o.edges();
+
+	o.translate(1.0,1.0,1.0);
+	o.vertices();
+
+	o.scale(2.0);
+	o.vertices();
+
+	o.rotate_z(3.141525);
+    o.vertices();
 }
